0620b4f4336f26a1c47b22e3b2d8c996311006a0
kazumaban <1310350007t@gmail.com>
2020-11-06 20:16:24 +0900

    second commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,5 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!
-  before_action :move_to_index, except: [:index, :show]
-
   
   def index
     @prototype = Prototype.all
@@ -28,6 +26,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
@@ -50,12 +51,4 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-
-  def move_to_index
-    prototype = Prototype.find(params[:id])
-    unless user_signed_in? && current_user.id == prototype.user_id
-      redirect_to action: :index
-    end
-  end
-
 end
